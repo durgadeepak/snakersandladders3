85394951a4c78de3697450662d76c54543683006
durgadeepak <104149761+durgadeepak@users.noreply.github.com>
2022-06-05 13:13:46 +0530

    created fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def Fibonacci(n):
+    if n<0:
+      print("incorrect input")
+    elif n==0:
+        return 0
+    elif n==1 or n==2:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+print(Fibonacci(9))
+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
